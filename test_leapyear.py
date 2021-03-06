8469a68d330b740db78d1c3f4b0f236db73d1b4d
VirginiaLink <linkv@oregonstate.edu>
2021-03-06 12:25:47 -0800

    Created test for leap year program that checks difficult and easy values for years to test

---
+++ test_leapyear.py
@@ -0,0 +1,20 @@
+# Virginia Link
+# CS 362
+# 03/06/2021
+
+import unittest
+
+class Test(unittest.TestCase):
+
+    def test_easy1(self):
+        self.assertTrue(calc(4))
+    def test_easy2(self):
+        self.assertTrue(calc(5))
+    def test_hard1(self):
+        self.assertTrue(calc(1700))
+    def test_hard2(self):
+        self.assertTrue(calc(2012))
+
+
+if __name__ == '__main__':
+    unittest.main()
